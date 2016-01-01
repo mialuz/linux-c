79082d9d343c043af8a85e411f0129a8df9f0b42
mialuz <mialuz@126.com>
2016-01-01 11:43:37 +0800

    新年快乐！

---
+++ newfile1.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: newfile1.c
+	> Author: mialuz
+	> Mail: mialuz@126.com
+	> Created Time: 2016年01月01日 星期五 11时39分20秒
+ ************************************************************************/
+
+#include<stdio.h>
+main()
+{
+
+printf("happy new year!");
+
+printf("hello 2016!");
+}
+//printf("happy new year!");
